4603208c9cb77aef61cb81249595da3d8f384b55
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-05-20 11:16:54 -0400

    animations and button adjustments

--- connect4.js
+++ connect4.js
@@ -1,33 +1,29 @@
-/** Connect Four
- *
- * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
- * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
- */
+const width = 7
+const height = 6
 
-const WIDTH = 7
-const HEIGHT = 6
+let currPlayer = 1
+let board = []
 
-let currPlayer = 1 // active player: 1 or 2
-let board = [] // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
+// matrix array for storing piece locations
 const makeBoard = () => {
-  board = Array.from(Array(HEIGHT), () => new Array(WIDTH).fill(null))
+  board = Array.from(Array(height), () => new Array(width).fill(null))
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 const makeHtmlBoard = () => {
   const htmlBoard = document.getElementById('board')
 
+  // handle start button
+  const startBtn = document.getElementById('start-button')
+  startBtn.classList.add('start')
+  startBtn.addEventListener('click', startGame)
+
   // create the top row that will add game pieces on click
   const top = document.createElement('tr')
   top.setAttribute('id', 'column-top')
-  top.addEventListener('click', handleClick)
+  top.classList.add('disabled')
 
-  for (let x = 0; x < WIDTH; x++) {
+  for (let x = 0; x < width; x++) {
     // top row buttons
     const headCell = document.createElement('td')
     headCell.setAttribute('id', x)
@@ -36,20 +32,19 @@ const makeHtmlBoard = () => {
     const dropText = document.createElement('div')
     dropText.innerText = 'drop'
     headCell.append(dropText)
-
-    const dropButton = document.createElement('div')
-    dropButton.classList.add('hide')
-    dropButton.innerText = '⬇︎'
-    headCell.append(dropButton)
+    const dropBtn = document.createElement('div')
+    dropBtn.innerText = '⬇︎'
+    dropBtn.classList.add('arrow')
+    headCell.append(dropBtn)
 
     top.append(headCell)
   }
   htmlBoard.append(top)
 
   // create each cell with a unique id
-  for (let y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < height; y++) {
     const row = document.createElement('tr')
-    for (let x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < width; x++) {
       const cell = document.createElement('td')
       cell.setAttribute('id', `${y}-${x}`)
       row.append(cell)
@@ -60,7 +55,7 @@ const makeHtmlBoard = () => {
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 const findSpotForCol = (x) => {
-  for (let y = HEIGHT - 1; y >= 0; y--) {
+  for (let y = height - 1; y >= 0; y--) {
     if (board[y][x] !== null) {
       continue
     }
@@ -77,46 +72,65 @@ const placeInTable = (y, x) => {
   cell.append(piece)
 }
 
-/** endGame: announce game end */
+// start game
+const startGame = () => {
+  const top = document.getElementById('column-top')
+  top.addEventListener('click', handleClick)
+  top.classList.remove('disabled')
+
+  // add arrow animation
+  const arrows = document.querySelectorAll('.arrow')
+  arrows.forEach((arrow) => arrow.classList.add('animate'))
+
+  // update h3 text
+  const h3 = document.querySelector('h3')
+  h3.innerText = 'Click a drop button to make your move'
+
+  // update start button
+  const startBtn = document.getElementById('start-button')
+  startBtn.classList.remove('start')
+  startBtn.removeEventListener('click', startGame)
+  startBtn.classList.add('restart')
+  startBtn.addEventListener('click', restartGame)
+}
+
+// end game
 const endGame = (msg) => {
   const top = document.getElementById('column-top')
+  top.classList.add('disabled')
   top.removeEventListener('click', handleClick)
   alert(msg)
 
-  // create restart button in place of h3
-  const h3 = document.querySelector('#title h3')
-  h3.remove()
-  const title = document.getElementById('title')
-  const restartBtn = document.createElement('button')
-  restartBtn.setAttribute('id', 'restart')
-  restartBtn.innerText = 'Restart'
-  title.append(restartBtn)
-  restartBtn.addEventListener('click', restartGame)
+  // remove arrow animation
+  const arrows = document.querySelectorAll('.arrow')
+  arrows.forEach((arrow) => arrow.classList.remove('animate'))
+
+  // update h3 text
+  const h3 = document.querySelector('h3')
+  h3.innerText = 'Restart Game?'
 }
 
 // restart the game
 const restartGame = () => {
-  // cleanup restart button
-  const restartBtn = document.getElementById('restart')
-  restartBtn.removeEventListener('click', restartGame)
-  restartBtn.remove()
+  // add arrow animation
+  const arrows = document.querySelectorAll('.arrow')
+  arrows.forEach((arrow) => arrow.classList.add('animate'))
 
-  // add title h3 back
-  const title = document.getElementById('title')
-  const h3 = document.createElement('h3')
-  h3.innerText = 'Click drop to begin!'
-  title.append(h3)
+  // update h3 text
+  const h3 = document.querySelector('h3')
+  h3.innerText = 'Click a drop button to make your move'
 
   // clean up board
   const pieces = document.querySelectorAll('.piece')
   pieces.forEach((piece) => piece.remove())
   const top = document.getElementById('column-top')
+  top.classList.remove('disabled')
   top.addEventListener('click', handleClick)
   makeBoard()
   currPlayer = 1
 }
 
-/** handleClick: handle click of column top to play piece */
+// handle click of top row 
 const handleClick = (evt) => {
   // get x from ID of clicked cell
   const x = +evt.target.parentNode.id
@@ -149,26 +163,22 @@ const handleClick = (evt) => {
   }
 }
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
+// check for winner
 const checkForWin = () => {
   const _win = (cells) => {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
-        y < HEIGHT &&
+        y < height &&
         x >= 0 &&
-        x < WIDTH &&
+        x < width &&
         board[y][x] === currPlayer
     )
   }
 
   // check winning combinations
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
       // horizontal win
       const horiz = [
         [y, x],
